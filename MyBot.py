b5d67153926171b009db7857264a522891b594d4
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2018-11-30 22:59:40 -0600

    smart navigate now takes advantage of wraparound

--- MyBot.py
+++ MyBot.py
@@ -166,28 +166,19 @@ while True:
         game_map[ship.position.directional_offset(move)].mark_unsafe(ship)
 
     def smart_navigate(ship, destination):
-        x = 0
-        y=1
-
-        ship_position_tuple = ship.position.x, ship.position.y
-        destination_position_tuple = destination.x, destination.y
-
-        distance_tuple = calculate_distance_tuple(destination_position_tuple, ship_position_tuple)
-        abs_distance_tuple = abs(distance_tuple[x]), abs(distance_tuple[y])
-
         safe_directions = find_safe_directions()
-
-        if abs_distance_tuple[x] > abs_distance_tuple[y]:
-            move =  abs_distance_tuple[x] // distance_tuple[x], 0
-        else:
-            move =  0, abs_distance_tuple[y] // distance_tuple[y]
-
-        if move in safe_directions:
-            return move
-        elif len(safe_directions) == 0:
+        if len(safe_directions) == 0:
             return DIRECTION_STAY
-        else:
-            return random.choice(safe_directions)
+
+        min_distance = 1000
+        best_direction = random.choice(safe_directions)
+        for direction in safe_directions:
+            test_location = ship.position.directional_offset(direction)
+            distance = game_map.calculate_distance(test_location, destination)
+            if distance < min_distance:
+                min_distance = distance
+                best_direction = direction
+        return best_direction
 
     def calculate_distance_tuple(location_1, location_2):
         return location_1[0] - location_2[0], location_1[1] - location_2[1]
